d6344a5e1cec2da1b239ea7f8144142f7d62c655
Vadym <vad.evlanov@gmail.com>
2021-01-13 15:17:17 +0200

    refactor

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,23 +2,19 @@ import { connect } from 'react-redux';
 import Section from './Section';
 import Notification from './Notification';
 
-function Statistics({ state, total, percentage }) {
-  const names = Object.keys(state);
+function Statistics(statistic) {
+  const names = Object.keys(statistic).filter(item => item !== 'dispatch');
 
   return (
     <Section title={'Statistics'}>
-      {total > 0 ? (
+      {statistic.total > 0 ? (
         <>
           <ul>
             {names.map(name => (
               <li key={name}>
-                {name}: {state[name]}
+                {name}: {statistic[name]}
               </li>
             ))}
-          </ul>
-          <ul>
-            <li>total: {total}</li>
-            <li>percentage: {percentage}</li>
           </ul>
         </>
       ) : (
@@ -35,9 +31,11 @@ const percentage = (state, total) =>
   total > 0 ? Math.round((state.statistics.good / total) * 100) : null;
 
 const mapStateToProps = state => ({
-  state: state.statistics,
+  good: state.statistics.good,
+  neutral: state.statistics.neutral,
+  bad: state.statistics.bad,
   total: culcTotal(state),
   percentage: percentage(state, culcTotal(state)),
 });
 
-export default connect(mapStateToProps)(Statistics);
+export default connect(mapStateToProps, null)(Statistics);
